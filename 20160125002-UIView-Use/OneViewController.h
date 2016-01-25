d30db918798dc3d119a1c2dfe7536bfcc6d40f71
raineryue <jinlei_yue@sina.com>
2016-01-25 16:54:50 +0800

    UIView的创建和使用
    UIView的创建和使用

--- 20160125002-UIView-Use/OneViewController.h
+++ 20160125002-UIView-Use/OneViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  OneViewController.h
 //  20160125002-UIView-Use
 //
 //  Created by Rainer on 16/1/25.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface OneViewController : UIViewController
 
 @end
-
